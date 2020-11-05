35c05aab679f5e70c7c293a24d3f96523d658c2e
VrushaliTandel <61860669+VrushaliTandel@users.noreply.github.com>
2020-11-05 08:38:44 +0530

    Add files via upload

--- games.py
+++ games.py
@@ -4,36 +4,33 @@ import os
 
 root = tk.Tk()
 
-root.geometry('1200x550')
+root.geometry('850x550')
+
 
 v = tk.StringVar()
 
 def result():
    os.system(f"python -m freegames.{v.get()}")
 
+tk.Label(root,text="""Choose One Game to Play:""",justify=tk.LEFT,padx=30,font='Helvetica 20 bold').pack()
 
-tk.Label(root,text="""Choose your favourite programming language:""",justify=tk.LEFT,padx=20).pack()
-
+img=PhotoImage(file="C:/Users/Dell/Desktop/SGP-7sem/play.png")
 img1=PhotoImage(file="C:/Users/Dell/Desktop/SGP-7sem/img1.png")
-img2=PhotoImage(file="C:/Users/Dell/Desktop/SGP-7sem/img1.png")
-img3=PhotoImage(file="C:/Users/Dell/Desktop/SGP-7sem/img1.png")
-img4=PhotoImage(file="C:/Users/Dell/Desktop/SGP-7sem/img1.png")
+img2=PhotoImage(file="C:/Users/Dell/Desktop/SGP-7sem/img2.png")
+img3=PhotoImage(file="C:/Users/Dell/Desktop/SGP-7sem/img3.png")
+img4=PhotoImage(file="C:/Users/Dell/Desktop/SGP-7sem/img4.png")
 
 
-tk.Radiobutton(root,image=img1,padx=20,pady=40,height=120,variable=v,value='snake').pack()
-tk.Label(root,text="""Snake""",justify=tk.LEFT,padx=20).pack()
+tk.Radiobutton(root,image=img1,padx=20,pady=40,height=80,variable=v,value='snake').pack()
+tk.Radiobutton(root,image=img2,padx=20,pady=40,height=80,variable=v,value='pacman').pack()
+tk.Radiobutton(root,image=img3,padx=20,pady=40,height=80,variable=v,value='simonsays').pack()
+tk.Radiobutton(root,image=img4,padx=20,pady=40,height=80,variable=v,value='memory').pack()
 
-tk.Radiobutton(root,image=img2,padx=20,pady=40,height=120,variable=v,value='pacman').pack()
-tk.Label(root,text="""Pacman""",justify=tk.LEFT,padx=20).pack()
-
-tk.Radiobutton(root,image=img3,padx=20,pady=40,height=120,variable=v,value='simonsays').pack()
-tk.Label(root,text="""Simonsays""",justify=tk.LEFT,padx=20).pack()
-
-tk.Radiobutton(root,image=img4,padx=20,pady=40,height=120,variable=v,value='memory').pack()
-tk.Label(root,text="""Memory""",justify=tk.LEFT,padx=20).pack()
-
-
-button = tk.Button(root, text='Play', width=25, command=result)
+button = tk.Button(root, text='Play',padx=80, width=75,height=75, command=result)
+button.config(image=img)
 button.pack()
 
 root.mainloop()
+
+
+
--- main.py
+++ main.py
@@ -5,12 +5,17 @@ import webbrowser
 import wikipedia #pip install wikipedia
 import os
 import random
+import smtplib
+
+email={"abc":"vrushalitandel2@gmail.com","xyz":"vrushalitandel1@gmail.com"}
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 
-engine.setProperty('voices', voices[0].id)
+## goto
 
+engine.setProperty('voices', voices[1].id)
+#id=0
 
 def speak(audio):
     engine.say(audio)
@@ -37,7 +42,6 @@ def takecommand():
     with sr.Microphone() as source:
         print("Listening...")
         r.pause_threshold = 1
-        r.energy_threshold = 50
         audio = r.listen(source)
     try:
         print("Recognizing...")
@@ -48,6 +52,16 @@ def takecommand():
     except Exception:
         print("Say that again please...")
         return "None"
+
+def sendEmail(to,msg):
+    file1 = open("C:\\Users\\Dell\\Desktop\\SGP-7sem\\p.txt", "r")
+    password=file1.read()
+    server=smtplib.SMTP('smtp.gmail.com',587)
+    server.ehlo()
+    server.starttls()
+    server.login("vrushalitandel2@gmail.com",password)
+    server.sendmail("vrushalitandel2@gmail.com",to,msg)
+    server.close()
 
 
 if __name__ == "__main__":
@@ -97,18 +111,60 @@ if __name__ == "__main__":
         elif 'wikipedia' in query:
             speak('searching...')
             query = query.replace("wikipedia", "").replace("search", "")
-            result = wikipedia.summary(query, sentences=2)
+            result = wikipedia.summary(query, sentences=1)
             speak("According to wikipedia")
             print(result)
             speak(result)
 
         elif 'game' in query:
-            os.system("python -m freegames.snake")
+            os.system("python games.py")
 
-        elif query!="none":
+        elif 'send email' in query:
+            try:
+                speak("Whom should i send?, Tell me the name of a person.")
+                name="none"
+                while name=="none":
+                    name = takecommand().lower()
+                    if name not in email:
+                        name="none"
+                        speak("I coudn't find the Person name,Say that again please")
+                    else:
+                        to = email[name]
+
+                speak("What should i say?")
+                text="None"
+                while text=="None":
+                    speak("Cannot be none")
+                    text = takecommand()
+
+                msg="none"
+                speak("Do you want to add Subject??say yes or no")
+                while msg == "none":
+                    sub=takecommand().lower()
+                    if sub == "no":
+                        msg = text
+                    elif sub == "yes":
+                        speak("Tell me the subject!")
+                        Subject="none"
+                        while Subject=="none":
+                            Subject = takecommand().lower()
+                            if Subject=="none":
+                                speak("I Coudn't recognize,Please say that again")
+                        msg = 'Subject: {}\n\n{}'.format(Subject, text)
+
+                print("Sending...")
+                speak("Sending, Please wait")
+                sendEmail(to, msg)
+                speak("Email has been sent!")
+
+            except Exception:
+                speak("There is an error occured and email has not been sent!")
+
+
+        elif query!="none" and 'exit' not in query:
             speak("searching...")
             query = query.replace("on google", "").replace("search", "")
-            webbrowser.open(f'https://www.google.com/search?q={query}&rlz=1C1CHBD_enIN824IN824&oq={query}&aqs=chrome..69i57j0l7.1579j0j15&sourceid=chrome&ie=UTF-8')
+            webbrowser.open(f'https://www.google.com/search?q={query}')
 
         elif 'exit' in query:
             exit()
